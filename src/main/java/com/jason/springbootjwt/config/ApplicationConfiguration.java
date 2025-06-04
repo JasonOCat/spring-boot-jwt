35f04d850efe979a5e74acf3f602bbd3a8e03219
JasonOCat <jason.catbagan@gmail.com>
2025-06-04 08:05:20 +0200

    Add AuthenticationManager with the custom authentication provider for improved integration.

--- src/main/java/com/jason/springbootjwt/config/ApplicationConfiguration.java
+++ src/main/java/com/jason/springbootjwt/config/ApplicationConfiguration.java
@@ -4,8 +4,10 @@ import com.jason.springbootjwt.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -28,6 +30,15 @@ public class ApplicationConfiguration {
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(userDetailsService);
         authProvider.setPasswordEncoder(passwordEncoder());
         return authProvider;
+    }
+
+    /*
+    The `AuthenticationManager` bean is used when calling `authenticationManager.authenticate()`
+    in the application.
+     */
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager(); // the default authenticationManager with my custom provider
     }
 
     @Bean
