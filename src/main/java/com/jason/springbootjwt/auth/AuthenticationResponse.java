ee6d8ba1ae32191544a804d0bc8bbbe144daf3bb
JasonOCat <jason.catbagan@gmail.com>
2025-06-04 08:33:52 +0200

    Handle authentication logic, including user registration and JWT token generation.

--- src/main/java/com/jason/springbootjwt/auth/AuthenticationResponse.java
+++ src/main/java/com/jason/springbootjwt/auth/AuthenticationResponse.java
@@ -1,7 +1,9 @@
 package com.jason.springbootjwt.auth;
 
+import lombok.Builder;
 import lombok.NonNull;
 
+@Builder
 public record AuthenticationResponse(
         @NonNull String token
 ) {
---
+++ src/main/java/com/jason/springbootjwt/auth/AuthenticationService.java
@@ -0,0 +1,42 @@
+package com.jason.springbootjwt.auth;
+
+
+import com.jason.springbootjwt.jwt.JwtService;
+import com.jason.springbootjwt.user.Role;
+import com.jason.springbootjwt.user.User;
+import com.jason.springbootjwt.user.UserRepository;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final JwtService jwtService;
+
+    public @NonNull AuthenticationResponse register(@NonNull RegisterRequest request) {
+        User user = User.builder()
+                .firstname(request.firstname())
+                .lastname(request.lastname())
+                .email(request.email())
+                .password(passwordEncoder.encode(request.password()))
+                .role(Role.USER)
+                .build();
+
+        userRepository.save(user);
+
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public @NonNull AuthenticationResponse authenticate(@NonNull AuthenticationRequest request) {
+    }
+}
